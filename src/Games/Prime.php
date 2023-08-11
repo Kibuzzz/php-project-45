8810595cf795eba6730c3cfcde11fca79daeb7a6
kibuzz <kibuzz15@gmail.com>
2023-08-11 13:43:10 +0300

    Made files and dependencies for Prime game

---
+++ src/Games/Prime.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Brain\Games\Prime;
+
+use function Brain\Engine\runGame;
+use function Brain\Engine\getNumber;
+
+use const Brain\Engine\ROUNDS;
+
+const INSTRUCTION = "Answer \"yes\" if given number is prime. Otherwise answer \"no\".\n";
