8953ae98825e93e2e02093669c0d5fd32e29bb06
kibuzz <kibuzz15@gmail.com>
2023-08-11 15:46:35 +0300

    Add myGcd function to Gcd.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -9,6 +9,14 @@ use const Brain\Engine\ROUNDS;
 
 const INSTRUCTION = "Find the greatest common divisor of given numbers.\n";
 
+function myGcd(int $number1, int $number2): int
+{
+    if ($number1 === 0) {
+        return $number2;
+    }
+    return myGcd($number2 % $number1, $number1);
+}
+
 function run(): void
 {
     $roundCount = 0;
@@ -17,7 +25,7 @@ function run(): void
         $guessNumber1 = getNumber();
         $guessNumber2 = getNumber();
         $questionString = "$guessNumber1 $guessNumber2";
-        $rightAnswer = gmp_gcd($guessNumber1, $guessNumber2);
+        $rightAnswer = myGcd($guessNumber1, $guessNumber2);
         $gameData[] = [$questionString, $rightAnswer];
         $roundCount++;
     }
